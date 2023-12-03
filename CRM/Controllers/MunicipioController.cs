f728226c07e4ecc1168d0b6fe94a7a1c590a0094
Santana <dnsilva.san@gmail.com>
2023-12-03 11:21:19 -0300

    melhorias

---
+++ CRM/Controllers/MunicipioController.cs
@@ -0,0 +1,28 @@
+﻿using CRM.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    [Route("api/[controller]"), ApiController, Authorize]
+    public class MunicipioController : BaseController
+    {
+        private readonly IMunicipioService municipioService;
+
+        public MunicipioController(IMunicipioService municipioService)
+        {
+            this.municipioService = municipioService;
+        }
+        [HttpGet, AllowAnonymous]
+        public IActionResult Get()
+        {
+            return Ok(municipioService.GetAll());
+        }
+
+        [HttpGet("{id}"), AllowAnonymous]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok(municipioService.GetById(id.ToString()));
+        }
+    }
+}
---
+++ CRM/Controllers/PaisController.cs
@@ -0,0 +1,28 @@
+﻿using CRM.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    [Route("api/[controller]"), ApiController, Authorize]
+    public class PaisController : BaseController
+    {
+        private readonly IPaisService paisService;
+
+        public PaisController(IPaisService paisService)
+        {
+            this.paisService = paisService;
+        }
+        [HttpGet, AllowAnonymous]
+        public IActionResult Get()
+        {
+            return Ok(paisService.GetAll());
+        }
+
+        [HttpGet("{id}"), AllowAnonymous]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok(paisService.GetById(id.ToString()));
+        }
+    }
+}
