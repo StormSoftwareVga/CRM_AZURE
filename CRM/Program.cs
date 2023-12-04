abf42438e850451835e2d2ac76012d895eae5034
Santana <dnsilva.san@gmail.com>
2023-12-04 11:34:49 -0300

    Aplicação de migration ao subir a aplicação

--- CRM/Program.cs
+++ CRM/Program.cs
@@ -84,4 +84,20 @@ app.UseEndpoints(endpoints =>
         pattern: "{controller}/{action=Index}/{id?}");
 });
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var dbContext = services.GetRequiredService<CRMDbContext>();
+        dbContext.Database.Migrate();         
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+    }
+}
+
 app.Run();
